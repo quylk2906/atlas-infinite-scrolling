dacb8fcf320056bfb6f76da6119842aa0f2020e5
quylk <quylk@gmail.com>
2024-07-26 22:02:03 +0700

    Fix html file

---
+++ src/ui/components/product.tsx
@@ -0,0 +1,338 @@
+import { FC } from 'react';
+
+const Product: FC = () => {
+  return (
+    <div className="products wrapper grid products-grid">
+      <div className="products list items product-items">
+        <li className="item product product-item">
+          <div
+            data-product-id={4522}
+            className="cdz-product-top amasty-label-for-4522  product-configurable"
+          >
+            <div className="product-item-info">
+              <div className="product-wrapper">
+                <div
+                  className="product-option-item active"
+                  data-color={5659}
+                  data-product-id={4209}
+                >
+                  <div className="data-variant-image">
+                    <a
+                      href="javascript:void(0)"
+                      className="action towishlist"
+                      title="Add to Wish List"
+                      aria-label="Save"
+                      role="button"
+                      data-post='{"action":"https:\/\/www.arielbath.com\/wishlist\/index\/add\/","data":{"product":4209,"uenc":"aHR0cHM6Ly93d3cuYXJpZWxiYXRoLmNvbS92YW5pdGllcw,,"}}'
+                      data-action="add-to-wishlist"
+                    >
+                      <span>Add to Wish List</span>
+                    </a>
+                    <a className="product photo product-item-photo">
+                      <img
+                        src="/images/ariel (1).jpg"
+                        data-thumbnail=""
+                        loading="lazy"
+                      />
+                      <img
+                        src="./Bathroom Vanities & Vanity Tops - Collection_files/t049scw2rvowht-blk_lifestyle.jpg"
+                        data-lifestyle=""
+                        loading="lazy"
+                        style={{ display: 'none', opacity: '0.00700198' }}
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div
+                  className="product-option-item"
+                  data-color={5658}
+                  data-product-id={4203}
+                >
+                  <div className="data-variant-image">
+                    <a
+                      href="javascript:void(0)"
+                      className="action towishlist"
+                      title="Add to Wish List"
+                      aria-label="Save"
+                      role="button"
+                      data-post='{"action":"https:\/\/www.arielbath.com\/wishlist\/index\/add\/","data":{"product":4203,"uenc":"aHR0cHM6Ly93d3cuYXJpZWxiYXRoLmNvbS92YW5pdGllcw,,"}}'
+                      data-action="add-to-wishlist"
+                    >
+                      <span>Add to Wish List</span>
+                    </a>
+                    <a className="product photo product-item-photo">
+                      <img
+                        src="./Bathroom Vanities & Vanity Tops - Collection_files/T049SCW2RVOGRY_productimage.jpg"
+                        data-thumbnail=""
+                        loading="lazy"
+                      />
+                      <img
+                        src="./Bathroom Vanities & Vanity Tops - Collection_files/t049scw2rvogry-blk_lifestyle.jpg"
+                        data-lifestyle=""
+                        loading="lazy"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div
+                  className="product-option-item"
+                  data-color={5682}
+                  data-product-id={4206}
+                >
+                  <div className="data-variant-image">
+                    <a
+                      href="javascript:void(0)"
+                      className="action towishlist"
+                      title="Add to Wish List"
+                      aria-label="Save"
+                      role="button"
+                      data-post='{"action":"https:\/\/www.arielbath.com\/wishlist\/index\/add\/","data":{"product":4206,"uenc":"aHR0cHM6Ly93d3cuYXJpZWxiYXRoLmNvbS92YW5pdGllcw,,"}}'
+                      data-action="add-to-wishlist"
+                    >
+                      <span>Add to Wish List</span>
+                    </a>
+                    <a className="product photo product-item-photo">
+                      <img
+                        src="./Bathroom Vanities & Vanity Tops - Collection_files/T049SCW2RVOMNB_productimage.jpg"
+                        data-thumbnail=""
+                        loading="lazy"
+                      />
+                      <img
+                        src="./Bathroom Vanities & Vanity Tops - Collection_files/t049scw2rvomnb-blk_lifestyle.jpg"
+                        data-lifestyle=""
+                        loading="lazy"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div
+                  className="product-option-item"
+                  data-color={6024}
+                  data-product-id={7649}
+                >
+                  <div className="data-variant-image">
+                    <a
+                      href="javascript:void(0)"
+                      className="action towishlist"
+                      title="Add to Wish List"
+                      aria-label="Save"
+                      role="button"
+                      data-post='{"action":"https:\/\/www.arielbath.com\/wishlist\/index\/add\/","data":{"product":7649,"uenc":"aHR0cHM6Ly93d3cuYXJpZWxiYXRoLmNvbS92YW5pdGllcw,,"}}'
+                      data-action="add-to-wishlist"
+                    >
+                      <span>Add to Wish List</span>
+                    </a>
+                    <a className="product photo product-item-photo">
+                      <img
+                        src="./Bathroom Vanities & Vanity Tops - Collection_files/t049scwrvowoa_productimage.jpg"
+                        data-thumbnail=""
+                        loading="lazy"
+                      />
+                      <img
+                        src="./Bathroom Vanities & Vanity Tops - Collection_files/t049scwrvowoa_productimage.jpg"
+                        data-lifestyle=""
+                        loading="lazy"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="product-option-item active"
+                data-product-id={4209}
+              >
+                <div className="product details product-item-details">
+                  <div
+                    className="price-box price-final_price"
+                    data-role="priceBox"
+                    data-product-id={4209}
+                    data-price-box="product-id-4209"
+                  >
+                    <span className="special-price">
+                      <span
+                        id="product-price-4209"
+                        data-price-amount={1522}
+                        data-price-type="finalPrice"
+                        className="price-wrapper "
+                      >
+                        $1,522.00
+                      </span>
+                    </span>
+                    <span className="old-price">
+                      <span
+                        id="old-price-4209"
+                        data-price-amount={2029}
+                        data-price-type="oldPrice"
+                        className="price-wrapper "
+                      >
+                        $2,029.00
+                      </span>
+                    </span>
+                  </div>{' '}
+                  <strong className="product name product-item-name">
+                    <a className="product-item-link">
+                      <span>
+                        Ariel hepburn 49 in. rectangle sink vanity with carrara
+                        white countertop in white
+                      </span>
+                    </a>
+                  </strong>
+                </div>
+              </div>
+              <div className="product-option-item" data-product-id={4203}>
+                <div className="product details product-item-details">
+                  <div
+                    className="price-box price-final_price"
+                    data-role="priceBox"
+                    data-product-id={4203}
+                    data-price-box="product-id-4203"
+                  >
+                    <span className="special-price">
+                      <span
+                        id="product-price-4203"
+                        data-price-amount={1522}
+                        data-price-type="finalPrice"
+                        className="price-wrapper "
+                      >
+                        $1,522.00
+                      </span>
+                    </span>
+                    <span className="old-price">
+                      <span
+                        id="old-price-4203"
+                        data-price-amount={2029}
+                        data-price-type="oldPrice"
+                        className="price-wrapper "
+                      >
+                        $2,029.00
+                      </span>
+                    </span>
+                  </div>{' '}
+                  <strong className="product name product-item-name">
+                    <a className="product-item-link">
+                      <span>
+                        Ariel hepburn 49 in. rectangle sink vanity with carrara
+                        white countertop in grey
+                      </span>
+                    </a>
+                  </strong>
+                </div>
+              </div>
+              <div className="product-option-item" data-product-id={4206}>
+                <div className="product details product-item-details">
+                  <div
+                    className="price-box price-final_price"
+                    data-role="priceBox"
+                    data-product-id={4206}
+                    data-price-box="product-id-4206"
+                  >
+                    <span className="special-price">
+                      <span
+                        id="product-price-4206"
+                        data-price-amount={1522}
+                        data-price-type="finalPrice"
+                        className="price-wrapper "
+                      >
+                        $1,522.00
+                      </span>
+                    </span>
+                    <span className="old-price">
+                      <span
+                        id="old-price-4206"
+                        data-price-amount={2029}
+                        data-price-type="oldPrice"
+                        className="price-wrapper "
+                      >
+                        $2,029.00
+                      </span>
+                    </span>
+                  </div>{' '}
+                  <strong className="product name product-item-name">
+                    <a className="product-item-link">
+                      <span>
+                        Ariel hepburn 49 in. rectangle sink vanity with 0.75 in.
+                        carrara white countertop in midnight blue
+                      </span>
+                    </a>
+                  </strong>
+                </div>
+              </div>
+              <div className="product-option-item" data-product-id={7649}>
+                <div className="product details product-item-details">
+                  <div
+                    className="price-box price-final_price"
+                    data-role="priceBox"
+                    data-product-id={7649}
+                    data-price-box="product-id-7649"
+                  >
+                    <span
+                      id="product-price-7649"
+                      data-price-amount={2021}
+                      data-price-type="finalPrice"
+                      className="price-wrapper "
+                    >
+                      $2,021.00
+                    </span>
+                  </div>{' '}
+                  <strong className="product name product-item-name">
+                    <a className="product-item-link">
+                      <span>
+                        Ariel hepburn 49 inch single rectangle sink bathroom
+                        vanity with italian carrara marble countertop 1.5 inch
+                        edge in white oak
+                      </span>
+                    </a>
+                  </strong>
+                </div>
+              </div>
+              <div className="product-options">
+                <div className="option-bullets color">
+                  <span
+                    className="option-bullet
+                                                                           active"
+                    data-product-id={4209}
+                    style={{ backgroundColor: '#f5f5f5' }}
+                  />
+                  <span
+                    className="option-bullet
+                                                                          "
+                    data-product-id={4203}
+                    style={{ backgroundColor: '#cccccc' }}
+                  />
+                  <span
+                    className="option-bullet
+                                                                          "
+                    data-product-id={4206}
+                    style={{ backgroundColor: '#11074f' }}
+                  />
+                  <span
+                    className="option-bullet
+                                                                          "
+                    data-product-id={7649}
+                    style={{
+                      backgroundImage:
+                        'url("https://www.arielbath.com/media/attribute/swatch/w/h/whiteoak_2.png")',
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <div
+          className="load-more-btn"
+          id="ias_loadMoreBtn_1721982346582"
+          style={{}}
+        >
+          <span className="current-showing">Showing 54 of 332</span>
+          <button className="view-more-button">View more products</button>
+        </div>
+      </div>
+      <div id="amasty-shopby-overlay" className="amshopby-overlay-block">
+        <span className="amshopby-loader" />
+      </div>
+    </div>
+  );
+};
+
+export default Product;
